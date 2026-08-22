4b4645e5514caec2b8f3a672366cb6ac060b6f87
Anderson Jordão Marques <anjomar@gmail.com>
2026-08-22 20:10:02 -0300

    Correção depreciados

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,14 @@
 import { TestBed, waitForAsync } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
         AppComponent
       ],
+      providers: [provideRouter([])]
     }).compileComponents();
   }));
 
